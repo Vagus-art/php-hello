46f166df996e05b118956959546d404bae295808
Vagus-art <agustin.favo@gmail.com>
2020-04-10 00:07:01 -0300

    first php json

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo "hello world";
+$data = array(
+    'name' => 'Agus',
+    'phone' => '13123123',
+    'gender' => 'male',
+    'nationality' => 'AR'
+);
+$json = json_encode($data);
+header ("Content-Type: application/json");
+echo $json;
